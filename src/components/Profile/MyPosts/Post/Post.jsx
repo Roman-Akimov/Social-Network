3beb5a4faa6a75b359c9671170a8092db92ae653
Roman-Akimov <roma_akimov_12@mail.ru>
2025-04-21 16:54:31 +0400

    15. change components

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from "./Post.module.css";
+
+
+function Post() {
+    return (
+        <div className={s.item}>
+            <img src='https://i.pinimg.com/736x/23/d9/2a/23d92af013c486ae8c11d2814d9affa0.jpg'/>
+            post 1
+            <div>
+                <span>like</span>
+            </div>
+        </div>
+
+    );
+}
+
+export default Post;

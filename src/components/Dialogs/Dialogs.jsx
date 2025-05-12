1e9529b0497e8b8c4157480e9f14a212b13f06de
Roman-Akimov <roma_akimov_12@mail.ru>
2025-05-12 18:06:41 +0400

    21. Обновление в Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,22 @@ import s from "./Dialogs.module.css"
 
 const Dialogs = (props) => {
     return (
-        <div>
-            hello
+        <div className={s.dialogs}>
+            {/*имена*/}
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Alexandr</div>
+                <div className={s.dialog}>Mark</div>
+                <div className={s.dialog}>Andrey</div>
+                <div className={s.dialog}>Yury</div>
+                <div className={s.dialog}>Jho</div>
+            </div>
+            {/*сообщения*/}
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>hello</div>
+                <div className={s.message}>How re u?</div>
+            </div>
+
         </div>
     )
 

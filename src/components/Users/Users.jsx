3a806da679bb70bf55b45b15c0439925aa7d665c
Roman-Akimov <roma_akimov_12@mail.ru>
2025-07-11 21:09:20 +0400

    49. Реализован Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,11 +1,40 @@
 import React from "react";
-
+import s from "./Users.module.css"
 let Users = (props) => {
-    return (
-        <div>TEXT</div>
-    )
 
+    if (props.users.length === 0){
+        props.setUsers([
+            {id: 1, userAvatar:"https://www.clipartmax.com/png/full/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.png",
+                follow: false, fullName: "David", status: "Im a programmer", location:{cityName: "Samara", countryName: "Russia"}},
+            {id: 2, userAvatar:"https://www.clipartmax.com/png/full/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.png",
+                follow: true, fullName: "John", status: "saller of people", location:{cityName: "New York", countryName: "USA"}},
+            {id: 3, userAvatar:"https://www.clipartmax.com/png/full/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.png",
+                follow: false, fullName: "Kira", status: "Junior Dota player", location:{cityName: "Minsk", countryName: "Belarus"}},])
+    }
 
+    return <div>
+        {
+            props.users.map(u => <div className={s.body} key={u.id}>
+                <span>
+                    <div className="s.userAvatar">
+                        <img src={u.userAvatar} alt="avatar"/>
+                    </div>
+                    <div>{
+                        u.follow
+                            ? (<button className={s.follow} onClick={() => { props.toggle(u.id) }}> Отписаться </button>)
+                            : (<button className={s.unfollow} onClick={() => { props.toggle(u.id) }}> Подписаться </button>)
+                    }
+                    </div>
+                    <span>
+                        <div>{u.fullName}</div><div>{u.status}</div>
+                    </span>
+                    <span>
+                        <div>{u.location.countryName}</div><div>{u.location.cityName}</div>
+                    </span>
+                </span>
+            </div>)
+        }
+    </div>
 }
 
 export default Users;
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,34 +1,38 @@
-const FOLLOW = "FOLLOW";
-const UNFOLLOW = "UNFOLLOW";
+
+const TOGGLE_FOLLOW = "TOGGLE_FOLLOW";
+const SET_USERS = "SET_USERS";
 
 let initialState = {
     users: [
-        {id: 1, follower: false, fullName: "David", status: "Im a programmer", local:{city: "Samara", country: "Russia"}},
-        {id: 1, follower: true, fullName: "John", status: "saller of people", local:{city: "New York", country: "USA"}},
-        {id: 1, follower: false, fullName: "Kira", status: "Junior Dota player", local:{city: "Minsk", country: "Belarus"}},
+
     ]
 };
 
 
-const usersReducer = (state = initialState, action) => {
+const usersReducer = (state = initialState, action = {}) => {
     switch (action.type){
-        case FOLLOW:
-            let stateCopy = {
+        case TOGGLE_FOLLOW:
+            return {
                 ...state,
-                //: [...state.users],
-                users: state.users.map(u => u){
-                    return  u
-        }
+                users: state.users.map(u =>
+                    u.id === action.userId
+                        ? {...u, follow: !u.follow}
+                        : u
+                )
             }
-        case UNFOLLOW:
+        case SET_USERS: {
+            return {
+                ...state,
+                users: [/*...state.users*/, ...action.users]
+            }
+        }
+
         default:
             return state
     }
 }
 
-export const followActionCreator = () => ({type: FOLLOW, UserId});
-export const unfollowActionCreator = () => ({type: UNFOLLOW});
-// export const updateNewPostActionCreator = (text) =>
-//     ({type: UPDATE_NEW_POST_TEXT, newText: text});
+export const toggleFollowAC = (userId) => ({type: TOGGLE_FOLLOW, userId});
+export const setUsersAC = (users) => ({type: SET_USERS, users});
 
-export default usersReducer();
+export default usersReducer;

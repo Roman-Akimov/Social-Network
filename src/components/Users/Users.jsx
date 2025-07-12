eefa18d118d58a91cdf37496a99aa7968d081ba0
Roman-Akimov <roma_akimov_12@mail.ru>
2025-07-12 17:09:48 +0400

    51. axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import s from "./Users.module.css"
+import axios from "axios";
 let Users = (props) => {
-
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        props.setUsers(response.data.items)
+    })
     if (props.users.length === 0){
-        props.setUsers([
-            {id: 1, userAvatar:"https://www.clipartmax.com/png/full/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.png",
-                follow: false, fullName: "David", status: "Im a programmer", location:{cityName: "Samara", countryName: "Russia"}},
-            {id: 2, userAvatar:"https://www.clipartmax.com/png/full/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.png",
-                follow: true, fullName: "John", status: "saller of people", location:{cityName: "New York", countryName: "USA"}},
-            {id: 3, userAvatar:"https://www.clipartmax.com/png/full/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.png",
-                follow: false, fullName: "Kira", status: "Junior Dota player", location:{cityName: "Minsk", countryName: "Belarus"}},])
+
     }
 
     return <div>
@@ -17,19 +14,19 @@ let Users = (props) => {
             props.users.map(u => <div className={s.body} key={u.id}>
                 <span>
                     <div className="s.userAvatar">
-                        <img src={u.userAvatar} alt="avatar"/>
+                        <img src={u.photos.small !== null? u.photos.small : "https://i.taplink.st/a/0/a/8/c/a68eab.jpg?2"} alt="avatar"/>
                     </div>
                     <div>{
-                        u.follow
-                            ? (<button className={s.follow} onClick={() => { props.toggle(u.id) }}> Отписаться </button>)
-                            : (<button className={s.unfollow} onClick={() => { props.toggle(u.id) }}> Подписаться </button>)
+                        u.followed
+                            ? (<button className={s.followed} onClick={() => { props.toggle(u.id) }}> Отписаться </button>)
+                            : (<button className={s.unfollowed} onClick={() => { props.toggle(u.id) }}> Подписаться </button>)
                     }
                     </div>
                     <span>
-                        <div>{u.fullName}</div><div>{u.status}</div>
+                        <div>{u.name}</div><div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.countryName}</div><div>{u.location.cityName}</div>
+                        <div>{/*u.location.countryName*/}</div><div>{/*u.location.cityName*/}</div>
                     </span>
                 </span>
             </div>)
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -4,7 +4,12 @@ const SET_USERS = "SET_USERS";
 
 let initialState = {
     users: [
-
+            {id: 1, userAvatar:"https://www.clipartmax.com/png/full/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.png",
+                followed: false, fullName: "David", status: "Im a programmer", location:{cityName: "Samara", countryName: "Russia"}},
+            {id: 2, userAvatar:"https://www.clipartmax.com/png/full/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.png",
+                followed: true, fullName: "John", status: "saller of people", location:{cityName: "New York", countryName: "USA"}},
+            {id: 3, userAvatar:"https://www.clipartmax.com/png/full/258-2582267_circled-user-male-skin-type-1-2-icon-male-user-icon.png",
+                followed: false, fullName: "Kira", status: "Junior Dota player", location:{cityName: "Minsk", countryName: "Belarus"}},
     ]
 };
 
@@ -16,7 +21,7 @@ const usersReducer = (state = initialState, action = {}) => {
                 ...state,
                 users: state.users.map(u =>
                     u.id === action.userId
-                        ? {...u, follow: !u.follow}
+                        ? {...u, followed: !u.followed}
                         : u
                 )
             }
